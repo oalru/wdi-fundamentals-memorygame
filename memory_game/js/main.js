0193efb4fe7cf9c8be86740aadd15d91d401f8f3
osama <osamaalruthaya@hotmail.com>
2019-09-15 16:06:52 +0300

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "queen";
+console.log("user flipped" + " " + cardFour);
+console.log("user flipped" + " " + cardTwo);
