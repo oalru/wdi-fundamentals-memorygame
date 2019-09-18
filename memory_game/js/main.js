549e3a0493e4f80ee43f06291ca3e3e8c476e44b
osama <osamaalruthaya@hotmail.com>
2019-09-18 06:06:08 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,18 @@
-console.log("Up and running!");
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "queen";
-console.log("user flipped" + " " + cardFour);
-console.log("user flipped" + " " + cardTwo);
+let cards = ["queen","queen","king","king"];
+let cardsInPlay = [];
+var cardOne = cards[0];
+    cardsInPlay.push('cardOne');
+        console.log("User flipped queen");
+var cardTwo = cards[1];
+    cardsInPlay.push('cardTwo');
+        console.log("User flipped king");
+    
+        if (cardsInPlay.length === 2){
+            cardsInPlay.length == 2;
+                if (cardsInPlay[0] === cardsInPlay[1]){
+                    alert("You found a match");
+                }
+                else  {
+                    alert("Sorry, try again");
+                }
+        }
